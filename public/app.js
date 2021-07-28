bf85e859d1184c34d73ca0621fae88b69389d41a
Naftali Weiss <Naftali@PROPUMP.local>
2021-07-28 13:30:14 -0400

    added click event to buttons

--- public/app.js
+++ public/app.js
@@ -3,5 +3,15 @@ const app = angular.module('WinRateApp', []);
 app.controller('WinController', ['$http', function($http){
     const ctrl = this;
 
+    this.addColor = (str) => {
+        console.log(str)
+    }
+
+    this.displayInputs = () => {
+        if(ctrl.name){
+            console.log(ctrl.name)
+            ctrl.name= ""
+        }
+    }
 
 }] );
