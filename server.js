5b4e99ba6979e455e9b25ecdbfd416087e48f8e4
Naftali Weiss <Naftali@PROPUMP.local>
2021-07-28 12:52:56 -0400

    making the repository

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+// const cors = require('cors');
+const app = express();
+
+const port = process.env.PORT || 3004
+
+// app.use(cors);
+app.use(express.json());
+app.use(express.static('public'));
+
+
+app.listen(port, () => console.log(`Hidee Hoe im on port ${port}`))
